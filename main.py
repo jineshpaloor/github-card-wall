2462c9ac70824efdc7c3df34b237a1ad29e7ed4a
Jinesh <jineshpaloor@gmail.com>
2015-07-31 16:49:50 +0530

    bootstraping done

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+# url_for('static', filename='style.css')
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html", name="Github Card Wall")
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/issue/<int:issue_id>')
+def show_issue(issue_id):
+    # show the post with the given id, the id is an integer
+    return 'Issue %d' % issue_id 
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/common.js
@@ -0,0 +1,3 @@
+(function() {
+    $("#id_element").datepicker({dateFormat: "yy-mm-dd"});
+})();
