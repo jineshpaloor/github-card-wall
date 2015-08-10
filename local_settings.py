ba3760c7663a6196e6e1c3e14a34eb30d04a01ee
Jinesh <jineshpaloor@gmail.com>
2015-08-10 19:20:31 +0530

    local settings added

---
+++ local_settings.py
@@ -0,0 +1,7 @@
+DATABASE_URI = 'sqlite:////tmp/github-flask.db'
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+DEBUG = True
+
+# Set these values
+GITHUB_CLIENT_ID = '6e64c0848143de395527'
+GITHUB_CLIENT_SECRET = 'b03748ac366d00b26beab0874863980575be3875'
