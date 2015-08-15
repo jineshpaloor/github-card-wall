5b9c04814bd5053c718190a04faf38bd890f925d
Jinesh <jineshpaloor@gmail.com>
2015-08-16 00:13:24 +0530

    adding forms

---
+++ forms.py
@@ -0,0 +1,6 @@
+from wtforms import Form, BooleanField, StringField, validators
+from wtforms import SelectMultipleField
+
+class ProjectForm(Form):
+    name = StringField('Name', [validators.Length(max=30, min=4), validators.InputRequired()])
+    repositories = SelectMultipleField('Repos', [validators.InputRequired()])
