5db5cdab1499b48ace5cac3bf726e3e0c4e86a94
Jinesh <jineshpaloor@gmail.com>
2015-08-17 00:34:02 +0530

    issue list preparation api call fix to avoid timeout

--- github_api.py
+++ github_api.py
@@ -35,19 +35,15 @@ def get_label_list(user, repo_name_list):
 def get_issue_dict(user, repo_list, lbl_list):
     github = Github(login_or_token=user.github_access_token)
     issues_dict = defaultdict(list)
-
     for repo_name in repo_list:
-        repo = get_repository(user, github,repo_name)
-        labels = [label for label in repo.get_labels()]
-
-        for label_name in lbl_list:
-            for label in labels:
-                if label.name == label_name:
-                    issue_list = [issue for issue in repo.get_issues(labels=[label])]
-                    for issue in issue_list:
-                        issues_dict[label_name].append(issue)
-    #logging.info(issues_dict)
+        repo = get_repository(user, github, repo_name)
+        issue_list = repo.get_issues()
+        for issue in issue_list:
+            for lbl in issue.labels:
+                if lbl.name in lbl_list:
+                    issues_dict[lbl.name].append(issue)
     return issues_dict
+
 
 def change_issue_label(user, lbl_from, lbl_to, repo_name, issue_id):
     github = Github(login_or_token=user.github_access_token)
