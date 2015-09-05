6165e1fc7b80e34f19141dff5e36799bdd8b4573
Jinesh <jineshpaloor@gmail.com>
2015-09-06 00:08:51 +0530

    db settings changed

--- local_settings.py
+++ local_settings.py
@@ -1,8 +1,9 @@
-#DATABASE_URI = 'sqlite:////tmp/github-flask.db'
-DATABASE_URI = "postgresql://localhost/cardwall"
-SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+import os
+DATABASE_URI = 'sqlite:////tmp/cardwall.db'
+#DATABASE_URI = "postgresql://localhost/cardwall"
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT' #os.urandom(32) #
 DEBUG = True
 
 # Set these values
-GITHUB_CLIENT_ID = '6e64c0848143de395527'
-GITHUB_CLIENT_SECRET = 'b03748ac366d00b26beab0874863980575be3875'
+GITHUB_CLIENT_ID = '6996e3232399d753d554' #'6e64c0848143de395527'
+GITHUB_CLIENT_SECRET = 'e0ec5f6129617ec01fa50a393b154bc3c306f2a2' #'b03748ac366d00b26beab0874863980575be3875'
